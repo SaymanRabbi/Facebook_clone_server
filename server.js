a97f1309270676ae552c28a1181450c6e0c00eec
sayman <mdrabbi.asm@gmail.com>
2022-08-02 20:54:42 +0600

    db connected

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+//cors
 const cors = require('cors')
+//mongoose
+const mongoose = require('mongoose')
 //dotenv
 const dotenv = require('dotenv')
-const {readdirSync} = require('fs')
+const { readdirSync } = require('fs')
+//app
 const app = express()
 //port
 const port = process.env.PORT || 4000
@@ -12,7 +16,12 @@ dotenv.config()
 app.use(cors())
 //Dynamic routes
 readdirSync("./routes").map(r => app.use("/", require("./routes/" + r)));
-
+//DataBase  
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('Database connected Successfully');
+}).catch(err => console.log(err.messages))
 //server Listening
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
