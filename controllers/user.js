2fefe8fece1a5131161cc7a9d457c31b14372c79
sayman <mdrabbi.asm@gmail.com>
2022-08-03 22:33:59 +0600

    emnail validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,44 @@
-exports.home = (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the API',
-        error: false
-    })
+const { validationEmail } = require('../helpers/validation')
+const User = require('../Models/User')
+exports.register = async(req, res) => {
+   try {
+    const {
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        bYear,
+        bMonth,
+        bDay,
+        gender
+       } = req.body
+//email validation
+if (!validationEmail(email)) {
+           return res.status(400).json({
+               messages:"Unvalid Email Address"
+           })
+       }
+const check = await User?.findOne({ email })
+       if (check) {
+        return res.status(400).json({
+        messages:"This email is already Exists Try Another One" 
+        })
+       }     
+     return  
+    const user = await new User({
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        bYear,
+        bMonth,
+        bDay,
+        gender
+    }).save()
+    res.json(user)
+   } catch (error) {
+    res.status(500).json({messages:error?.messages})
+   }
 }
---
+++ helpers/validation.js
@@ -0,0 +1,3 @@
+exports.validationEmail = (email) => {
+    return String(email).toLowerCase().match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/)
+}
