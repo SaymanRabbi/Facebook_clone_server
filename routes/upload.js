c24b9a5f7ef8bb0f22cc04ac0e5e4665dfeb8129
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-13 18:30:52 +0600

    add middleware when creating post

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {
-    upload
-} = require("../controllers/upload");
+const {upload} = require("../controllers/upload");
+const { authUser } = require("../Middlewers/auth");
 const imagesauth = require("../Middlewers/imagesauth");
-router.post("/upload",imagesauth,upload);
+router.post("/upload",authUser,imagesauth,upload);
 module.exports = router;
