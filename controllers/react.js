49ca1fcaee3f5f130ac27a399b65f4d0a632e2e3
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-05 21:56:42 +0600

    work all react

--- controllers/react.js
+++ controllers/react.js
@@ -44,13 +44,50 @@ exports.reactPost = async (req, res) => {
 exports.getReact=async (req, res) => {
 try {
     const {id} = req.params;
-        const reacts = await React.find({postRef:id});
+        const Reactarray = await React.find({postRef:id});
+        // console.log(Reactarray);
+        const newReacts = Reactarray.reduce((group, react) => {
+            let key = react["react"];
+            group[key] = group[key] || [];
+            group[key].push(react);
+            return group;
+          }, {});
+            // console.log(newReacts);
+const reacts = [
+    {
+        react: "like",
+        count: newReacts.like ? newReacts.like.length : 0,
+      },
+      {
+        react: "love",
+        count: newReacts.love ? newReacts.love.length : 0,
+      },
+      {
+        react: "haha",
+        count: newReacts.haha ? newReacts.haha.length : 0,
+      },
+      {
+        react: "sad",
+        count: newReacts.sad ? newReacts.sad.length : 0,
+      },
+      {
+        react: "wow",
+        count: newReacts.wow ? newReacts.wow.length : 0,
+      },
+      {
+        react: "angry",
+        count: newReacts.angry ? newReacts.angry.length : 0,
+      },
+]
+reacts.sort((a, b) => {
+    return b.count - a.count;
+  });
         const cheack = await React.findOne({
             postRef: id,
             userRef: req?.user?.id,
         })
-        // conslole.log(cheack?.react?.react);
-        res.status(200).json({reacts,cheack:cheack});
+        console.log(cheack);
+        res.status(200).json({reacts,cheack:cheack,total:Reactarray.length});
 } catch (error) {
     res.status(500).json({  messages:error.message});
 }
