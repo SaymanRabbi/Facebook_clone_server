86656703e35196f67f6f9b38ee95bd9fec033179
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-05 17:19:47 +0600

    React router

---
+++ Models/React.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+
+const reactSchema = new mongoose.Schema({
+  react:{
+    type: String,
+    enum:["like","love","haha","wow","sad","angry"],
+    required: true,
+  },
+  postRef:{
+    type: ObjectId,
+    ref: "Post",
+  },
+    userRef:{
+    type: ObjectId,
+    ref: "user",
+    }
+});
+
+module.exports = mongoose.model("React", reactSchema);
