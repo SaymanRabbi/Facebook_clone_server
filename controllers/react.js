323896df3c3a26c5b6cdb56e2dca971ba33d9f19
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-05 17:31:45 +0600

    find react and remove

--- controllers/react.js
+++ controllers/react.js
@@ -1,3 +1,35 @@
+const React = require("../Models/React");
+const mongoose = require("mongoose");
 exports.reactPost = async (req, res) => {
-    
+    try {
+        const { react, postRef } = req.body;
+        const cheack = await React.findOne({
+            postRef,
+            userRef: mongoose.Types.ObjectId(req?.user?.id),
+        })
+        if (cheack==null) {
+            const react = new React({
+                react,
+                postRef,
+                userRef: req?.user?.id,
+            });
+            await react.save();
+            res.status(200).json({
+                message: "Reacted",
+            });
+        }
+        else {
+            if(cheack.react==react){
+                await React.findByIdAndDelete(cheack._id);
+                res.status(200).json({
+                    message: "Unreacted",
+                });
+            }
+        }
+
+    } catch (error) {
+       
+        res.status(500).json({  messages:error.message});
+        
+    }
 }
